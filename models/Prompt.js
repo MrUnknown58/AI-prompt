34005c93c1dc1fddcd6c3dbd4dc3ad7bd725bf71
unknown <sanusingh1603@gmail.com>
2023-05-12 14:33:39 +0530

    Schema changed

--- models/Prompt.js
+++ models/Prompt.js
@@ -18,29 +18,8 @@
 // const Prompt = models.Prompt || model("Prompt", PromptSchema);
 
 // export default Prompt;
-// import mongoose from "mongoose";
-// import User from "./User";
-
-// const PromptSchema = new mongoose.Schema({
-//   creator: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//   },
-//   prompt: {
-//     type: String,
-//     required: [true, "Prompt is required"],
-//   },
-//   tag: {
-//     type: String,
-//     required: [true, "Tag is required"],
-//   },
-// });
-
-// const Prompt = mongoose.models.Prompt || mongoose.model("Prompt", PromptSchema);
-
-// export default Prompt;
-
 import mongoose from "mongoose";
+import User from "./User";
 
 const PromptSchema = new mongoose.Schema({
   creator: {
